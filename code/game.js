ea61379582d661b01b16327d6d92d6b13954fe7d
Henry-H08 <143623813+Henry-H08@users.noreply.github.com>
2023-09-17 14:41:53 -0400

    Update game.js

--- code/game.js
+++ code/game.js
@@ -1,17 +1,41 @@
-scene('game', () => {
-  const apple = add([
-    sprite('apple'),
-    pos(SCALE*3, SCALE*3),
-    scale((SCALE/64) * 3), // SCALE divided by sprite width, multiplied by the size change
-    rotate(0),
-    anchor('center'),
+// Adding game objects to screen
 
-  ]);
+// Start a kaboom game
+kaboom()
 
-  onUpdate(() => {
-    apple.angle += dt()*150;
-    apple.pos = mousePos();
-  });
-});
+// Load a sprite asset from "sprites/bean.png", with the name "bean"
+loadSprite("bean", "/sprites/bean.png")
+loadSprite("ghosty", "/sprites/ghosty.png")
 
-go('game');
+// A "Game Object" is the basic unit of entity in kaboom
+// Game objects are composed from components
+// Each component gives a game object certain capabilities
+
+// add() assembles a game object from a list of components and add to game, returns the reference of the game object
+const player = add([
+	sprite("bean"),   // sprite() component makes it render as a sprite
+	pos(120, 80),     // pos() component gives it position, also enables movement
+	rotate(0),        // rotate() component gives it rotation
+	anchor("center"), // anchor() component defines the pivot point (defaults to "topleft")
+])
+
+// .onUpdate() is a method on all game objects, it registers an event that runs every frame
+player.onUpdate(() => {
+	// .angle is a property provided by rotate() component, here we're incrementing the angle by 120 degrees per second, dt() is the time elapsed since last frame in seconds
+	player.angle += 120 * dt()
+})
+
+// Add multiple game objects
+for (let i = 0; i < 3; i++) {
+
+	// generate a random point on screen
+	// width() and height() gives the game dimension
+	const x = rand(0, width())
+	const y = rand(0, height())
+
+	add([
+		sprite("ghosty"),
+		pos(x, y),
+	])
+
+}
