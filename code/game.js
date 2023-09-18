5849e65d9a5c2d87786d39224fd45b6310a1a52a
Henry-H08 <143623813+Henry-H08@users.noreply.github.com>
2023-09-18 13:59:37 -0400

    Update game.js

--- code/game.js
+++ code/game.js
@@ -1,51 +1,3 @@
-// Input handling and basic player movement
-
-// Start kaboom
-kaboom()
-
-// Load assets
-loadSprite("bean", "sprites/bean.png")
-
-// Define player movement speed (pixels per second)
-const SPEED = 320
-
-// Add player game object
-const player = add([
-	sprite("bean"),
-	// center() returns the center point vec2(width() / 2, height() / 2)
-	pos(center()),
-])
-
-// onKeyDown() registers an event that runs every frame as long as user is holding a certain key
-onKeyDown("left", () => {
-	// .move() is provided by pos() component, move by pixels per second
-	player.move(-SPEED, 0)
-})
-
-onKeyDown("right", () => {
-	player.move(SPEED, 0)
-})
-
-onKeyDown("up", () => {
-	player.move(0, -SPEED)
-})
-
-onKeyDown("down", () => {
-	player.move(0, SPEED)
-})
-
-// onClick() registers an event that runs once when left mouse is clicked
-onClick(() => {
-	// .moveTo() is provided by pos() component, changes the position
-	player.moveTo(mousePos())
-})
-
-add([
-	// text() component is similar to sprite() but renders text
-	text("Press arrow keys", { width: width() / 2 }),
-	pos(12, 12),
-])
-
 kaboom()
 
 loadSprite("bean", "/sprites/bean.png")
@@ -73,7 +25,7 @@ for (let i = 12; i < 24; i++) {
 
 nucleus.onUpdate(() => {
 
-	nucleus.pos = playerPos()
+	nucleus.pos = mousePos()
 
 	// update children
 	nucleus.children.forEach((child) => {
